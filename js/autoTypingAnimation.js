18d37d634790f213ae6d33c71b0259ed45cceb55
AbdullahSiddiqui2002 <abdullah.siddiqui13122002@gmail.com>
2024-07-17 22:04:13 +0500

    dffffffffff

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Abdullah Siddiqui</span> <br><br> As a frontend developer with extensive experience, I specialize in crafting visually appealing and user-friendly websites. Proficient in HTML, CSS, JavaScript, Bootstrap, and jQuery, I have successfully delivered numerous projects, ensuring optimal functionality and aesthetics. <br><br> My backend skills in SQL Server and Asp.Net (MVC) complement my frontend expertise, enabling me to create seamless, end-to-end solutions. <br><br> With a track record of delivering high-quality work and a commitment to staying updated with the latest technologies, I am well-equipped to meet your project requirements effectively."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
